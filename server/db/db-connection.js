f8c603f5176e2b1dd46b482b75c5363e6d5e9afb
Fariba Dadko <faribacodes@gmail.com>
2022-03-21 22:43:02 -0700

    added server and db

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -1,6 +1,12 @@
+//importing the pg library BUT we are destructuring and
+//get an instance of pool
 const { Pool } = require('pg');
+
 const db = new Pool({
-    connectionString: process.env.DB_URI
-  });
+
+
+  connectionString: process.env.DB_URI
+})
+
 
   module.exports = db;
--- server/server.js
+++ server/server.js
@@ -3,53 +3,96 @@ const cors = require('cors');
 
 //FOR DEVELOPMENT or FOR PRODUCTION environment variables ===Environment variables are, in short, variables that describe the environment in which apps and programs run
 require('dotenv').config();
-const db = require('../server/db/db-connection.js');
+// const db = require('../server/db/db-connection.js');
+const db = require('./db/db-connection');
 
 //creating an instance of express
 const app = express();
 
-const PORT = 5005;
-app.use(cors());
-app.use(express.json());
+//if this env val is defined we set port to that value : else => 5005
 
-//creates an endpoint for the route /api
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from My ExpressJS' });
+const PORT = process.env.PORT || 5005;
+app.use(cors());
+app.use(express.json()); //built in => takes body information and attach to request BODY
+
+// GET - '/' creates an endpoint for the route /api
+app.get('/', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM animals');
+    console.log(results);
+    // const { rows: animals } = await db.query('SELECT * FROM animals'); //table name
+    res.send(results);
+  } catch (error) {
+    console.send(error);
+  }
 });
 
-//create the get request
-app.get('/api/myanimals', cors(), async (req, res) => {
-  //database
-  // const My = [
-
-  //     { id: 1, commonname: 'Lisa', scientificname: 'Lee', total_wild: },
-  //     { id: 2, firstName: 'Eileen', lastName: 'Long' },
-  //     { id: 3, firstName: 'Fariba', lastName: 'Dako' },
-  //     { id: 4, firstName: 'Cristina', lastName: 'Rodriguez' },
-  //     { id: 5, firstName: 'Andrea', lastName: 'Trejo' },
-  // ];
-  // res.json(STUDENTS);
+// // GET - ALL animals
+app.get('/animals', cors(), async (req, res) => {
   try {
-    const { rows: myanimals } = await db.query('SELECT * FROM animals'); //table name
-    res.send(myanimals);
+    //to send a query => db.query then the postgress
+    const results = await db.query('SELECT * FROM animals');
+    console.log(results);
+    // const { rows: animals } = await db.query('SELECT * FROM animals'); //table name
+    res.send(results);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
+//// GET - '/' create the get request
+app.get('/animals/:id', cors(), async (req, res) => {
+  try {
+    const { rows: animals } = await db.query('SELECT * FROM animals'); //table name
+    res.send(animals);
+    console.log(req.params);
   } catch (e) {
     return res.status(400).json({ e });
   }
 });
 
 //create the POST request
-app.post('/api/myanimals', cors(), async (req, res) => {
-  const newUser = {
-    commonname: req.body.firstname,
-    lastname: req.body.lastname,
+app.post('/api/animals', cors(), async (req, res) => {
+  const newAnimal = {
+    commonname: req.body.commonname,
+    scientificname: req.body.scientificname,
   };
-  console.log([newUser.firstname, newUser.lastname]);
+  console.log([newAnimal.commonname, newAnimal.scientificname]);
+
   const result = await db.query(
-    'INSERT INTO myanimals(firstname, lastname) VALUES($1, $2) RETURNING *',
-    [newUser.firstname, newUser.lastname]
+    'INSERT INTO myanimals(commonname, scientificname) VALUES($1, $2) RETURNING *',
+    [newAnimal.commonname, newAnimal.scientificname]
   );
   console.log(result.rows[0]);
   res.json(result.rows[0]);
+});
+
+//create the PUT request
+app.put('/api/animals/:id', cors(), async (req, res) => {
+  try {
+    const updatedAnimal = await req.params.id;
+
+    console.log([updatedAnimal.commonname, updatedAnimal.scientificname]);
+
+    const result = await db.query(
+      'INSERT INTO myanimals(commonname, scientificname) VALUES($1, $2) RETURNING *',
+      [updatedAnimal.commonname, updatedAnimal.scientificname]
+    );
+  } catch (error) {
+    console.log(result.rows[0]);
+    res.json(result.rows[0]);
+  }
+});
+
+//DELETE
+
+//create the PUT request
+app.delete('/api/animals/:id', cors(), async (req, res) => {
+  try {
+    const delteAnimal = await req.params.id;
+  } catch (error) {
+    console.log('success');
+  }
 });
 
 // console.log that your server is up and running
